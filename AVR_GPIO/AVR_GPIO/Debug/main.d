629da7842670a127f346892363aec3c7a0f9624e
Jiangtian Feng <yewhen0704@gmail.com>
2019-04-20 15:21:10 -0500

    successfully output the PWM signal

--- AVR_GPIO/AVR_GPIO/Debug/main.d
+++ AVR_GPIO/AVR_GPIO/Debug/main.d
@@ -1,4 +1,5 @@
 main.d main.o: .././main.c \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
@@ -12,8 +13,9 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -40,7 +42,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
--- AVR_GPIO/AVR_GPIO/main.c
+++ AVR_GPIO/AVR_GPIO/main.c
@@ -4,7 +4,7 @@
  * Created: 2019/4/20 4:22:31
  * Author : User
  */ 
-
+#include <math.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -22,6 +22,36 @@ ISR(TIMER1_COMPB_vect){
 	LED_OFF;
 }
 
+/*ISR(TIMER0_COMPA_vect){
+	uint16_t period = OCR1A;
+	uint16_t duty = OCR1B;
+
+	if (duty < period) duty++;
+	else duty = 0;
+
+	OCR1B = duty;
+
+}*/
+
+void PWM_Increse_duty(){
+	uint16_t period = OCR1A;
+	uint16_t duty = OCR1B;
+
+	if (duty < period) duty++;
+	else duty = 0;
+
+	OCR1B = duty;
+}
+
+void PWM_Decrease_duty(){
+	uint16_t period = OCR1A;
+	uint16_t duty = OCR1B;
+
+	if (duty > 0) duty--;
+	else duty = 0;
+
+	OCR1B = duty;
+}
 
 void Timer_Frequency(uint8_t freq){
 	TCCR1B |= (1<<CS12) | (1<<WGM12);
@@ -30,31 +60,38 @@ void Timer_Frequency(uint8_t freq){
 	OCR1A = (F_CPU / (freq * 2 * 256) - 1);	
 }
 
+void milliS_Timer(uint8_t milliS){
+	TCCR0A |= (1<<WGM01); // CTC mode
+	TCCR0B |= (1<<CS02) | (1<<CS00); //prescalar of 1024
+	OCR0A = milliS*7,8125-1;
+	TIMSK0 |= (1<<OCIE0A);
+}
+
 void PWM_Init(void){
-	TCCR1B |= (1<<CS10) | (1<<WGM12); // CTC mode
+	TCCR1B |= (1<<CS12) | (1<<WGM12); // CTC mode
 	TIMSK1 |= (1<<OCIE1A) | (1<<OCIE1B);
 	
-	OCR1A = 800; // (F_CPU / (10000*2*1) - 1)
-	OCR1B = 400;
+	OCR1A = (F_CPU / (40 * 2 * 256) - 1); // 16000000 / (20480)
+	OCR1B = 800*.5;
 }
 
 int main(void){
     /* Replace with your application code */
 	DDRB |= (1<<DDB5);
 	DDRB &= ~(1<<DDB7);
-	
+	//milliS_Timer(2);
 	//Timer_Frequency(1);
 	PWM_Init();
 	sei();
 	
     while (1){
 		
-		//if (!(PINB & (1<<PINB7))){
-		//	LED_ON;
-		//}
-		//else{
-		//	LED_OFF;
-		//}
+		/*if (!(PINB & (1<<PINB7))){
+			LED_ON;
+		}
+		else{
+			LED_OFF;
+		}*/
 	}
 	return 0;
 }
