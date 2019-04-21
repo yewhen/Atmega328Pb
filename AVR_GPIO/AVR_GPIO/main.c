691f8d698914003da7e9979b0085b9c3de91b6aa
Jiangtian Feng <yewhen0704@gmail.com>
2019-04-20 19:58:51 -0500

    successfully conmfigure the terminal window

--- AVR_GPIO/AVR_GPIO/main.c
+++ AVR_GPIO/AVR_GPIO/main.c
@@ -6,15 +6,19 @@
  */ 
 #include <math.h>
 #include <avr/io.h>
+//#include <USART_irq.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
-#define F_CPU 16000000UL // 16MHz
+
+#define BAUD				9600
+#define F_CPU				16000000UL // 16MHz
+#define MYUBBR				F_CPU/16/BAUD-1
 
 #define LED_ON				PORTB |= (1<<PORTB5)
 #define LED_OFF				PORTB &= ~(1<<PORTB5)
 #define LED_TOGGLE			PINB |= (1<<PINB5)
 
-ISR(TIMER1_COMPA_vect){
+/*ISR(TIMER1_COMPA_vect){
 	LED_ON;
 }
 
@@ -22,7 +26,7 @@ ISR(TIMER1_COMPB_vect){
 	LED_OFF;
 }
 
-/*ISR(TIMER0_COMPA_vect){
+ISR(TIMER0_COMPA_vect){
 	uint16_t period = OCR1A;
 	uint16_t duty = OCR1B;
 
@@ -75,15 +79,47 @@ void PWM_Init(void){
 	OCR1B = 800*.5;
 }
 
+void USART_Init(unsigned int ubrr){
+	/*Set baud rate */
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR0C = (3<<UCSZ00);
+}
+
+unsigned char USART_Receive(){
+	/* Wait for data to be received */
+	while (!(UCSR0A & (1<<RXC0)));
+	/* Get and return received data from buffer */
+	return UDR0;
+}
+
+void USART_Transmit(unsigned char data){
+	/* Wait for empty transmit buffer */
+	while (!(UCSR0A & (1<<UDRE0)));
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
+
+void USART_putstring(char* strptr){
+	while (*strptr != 0x00){
+		USART_Transmit(*strptr);
+		strptr++;
+	}
+}
+
 int main(void){
-    /* Replace with your application code */
-	DDRB |= (1<<DDB5);
-	DDRB &= ~(1<<DDB7);
-	//milliS_Timer(2);
-	//Timer_Frequency(1);
-	PWM_Init();
+    
+	//DDRB |= (1<<DDB5);
+	//DDRB &= ~(1<<DDB7);
 	sei();
-	
+	char str[] = "Hello World!";
+	USART_Init(MYUBBR);
+
+	USART_putstring(str);
+
     while (1){
 		
 		/*if (!(PINB & (1<<PINB7))){
